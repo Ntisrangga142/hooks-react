ba1d0a2226d46297c26eb9f8fe8d6dbfc15d3e37
Titus Rangga <titusrangga142@gmail.com>
2025-08-06 09:12:34 +0700

    fix: main component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Header from "./components/templates/Header";
+import LoginProvider from "./contexts/Login/LoginProvider";
+import TodoProvider from "./contexts/TodoList/TodoProvider";
+import Todo from "./components/pages/Todo.jsx";
+import Router from "./Router.jsx"
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <TodoProvider>
+      <LoginProvider>
+        {/* <Header /> */}
+        {/* <Todo /> */}
+        <Router />
+      </LoginProvider>
+    </TodoProvider>
+  </StrictMode>
+);
